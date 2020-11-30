9c8b6ed4e591091b7b99d2cdcda765a04dea886c
bembengggg <beng.1411050084@mail.darmajaya.ac.id>
2020-11-30 23:00:09 +0700

    text

--- src/Component/Gallery/Gallery.jsx
+++ src/Component/Gallery/Gallery.jsx
@@ -7,16 +7,21 @@ const Gallery =()=>{
     return(
         <div className='wrap'>
            <img className='imgbg' alt='imgbg'src={gallery}/>
-           <div className='slider'>
-               <div className='thumbnail nail-1'>1</div>
-               <div className='thumbnail nail-2'>2</div>
-               <div className='thumbnail nail-3'>3</div>
-               <div className='thumbnail nail-4'>4</div>
-               <div className='thumbnail nail-5'>5</div>
-               <div className='thumbnail nail-6'>6</div>
-               <div className='thumbnail nail-7'>7</div>
-               <div className='thumbnail nail-8'>8</div> 
-               <div className='thumbnail nail-9'>9</div>
+           <div className='grid'>
+           <img alt='imgbg'src={gallery}/>
+           <img alt='imgbg'src={gallery}/>
+           <img alt='imgbg'src={gallery}/>
+           <img alt='imgbg'src={gallery}/>
+           <img alt='imgbg'src={gallery}/>
+           <img alt='imgbg'src={gallery}/>
+           <img alt='imgbg'src={gallery}/>
+           <img alt='imgbg'src={gallery}/>
+           <img alt='imgbg'src={gallery}/>
+           <img alt='imgbg'src={gallery}/>
+           <img alt='imgbg'src={gallery}/>
+           <img alt='imgbg'src={gallery}/>
+            
+
            </div>
          </div>
     
