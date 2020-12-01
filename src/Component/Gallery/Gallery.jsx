97e984086ad10207435fd7de51890765ff76706d
bembengggg <beng.1411050084@mail.darmajaya.ac.id>
2020-12-01 10:37:28 +0700

    text

--- src/Component/Gallery/Gallery.jsx
+++ src/Component/Gallery/Gallery.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import './Gallery.css';
 import gallery from '../Gallery/bgimage.jpg';
-import bambang1 from '../../Assets/image/bambang1.jpg';
-import bambang2 from '../../Assets/image/bambang2.jpg';
-import bambang3 from '../../Assets/image/bambang3.jpg';
-import bambang4 from '../../Assets/image/bambang4.jpg';
-import bambang5 from '../../Assets/image/bambang5.jpg';
-import bambang6 from '../../Assets/image/bambang6.jpg';
-import bambang7 from '../../Assets/image/bambang7.jpg';
-import bambang8 from '../../Assets/image/bambang8.jpg';
-import bambang9 from '../../Assets/image/bambang9.jpg';
-import bambang10 from '../../Assets/image/bambang10.jpg';
-import bambang11 from '../../Assets/image/bambang11.jpg';
-import bambang12 from '../../Assets/image/bambang12.jpg';
+
+
 
 
 
